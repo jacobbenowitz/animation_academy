fbc8be90e4be1bbc11ee13f0777bb3452069c669
Jacob Benowitz <jacob.benowitz@gmail.com>
2022-04-02 15:47:45 -0400

    added scss styles, unable to import into main.css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // entry point, bundler will take required files here for use in main.js
 
 import Example from "./scripts/functionality.js"
+import NavBar from "./scripts/nav_bar"
 
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main")
     new Example(main)
+
+    const header = document.getElementById("header")
+    new NavBar(header)
 })
---
+++ src/scripts/nav_bar.js
@@ -0,0 +1,14 @@
+class NavBar {
+    constructor(ele){
+        this.ele = ele
+        this.ele.innerHTML = "<h1>It's ALIVE!!!</h1>"
+        // this.handleClick = this.handleClick.bind(this)
+        // this.ele.addEventListener("click", this.handleClick)
+    }
+
+    handleClick(){
+        this.ele.children[0].innerText = "Ouch!"
+    }
+}
+
+export default NavBar;
